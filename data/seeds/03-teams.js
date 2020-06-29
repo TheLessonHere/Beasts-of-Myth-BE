b175cfcb86693ec5bf92d4bd39bacefb2dca4da6
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2020-06-29 13:54:38 -0600

    Updated gitignore

---
+++ data/seeds/03-teams.js
@@ -0,0 +1,14 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('teams').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('teams').insert([
+        {user_id: 1,
+          team_datastring: "format]/team_name/>beast_name>||slot#||)item)}move1,move2,move3,move4}]"},
+          {user_id: 2,
+            team_datastring: "format]/team_name/>beast_name>||slot#||)item)}move1,move2,move3,move4}]"},
+      ]);
+    });
+};
