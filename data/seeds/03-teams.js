17757652f7692555bb5bfc3c1cf40cee2fb76a0d
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2020-06-29 16:17:25 -0600

    Attempting migrate without deletion

--- data/seeds/03-teams.js
+++ data/seeds/03-teams.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('teams').truncate()
+  return knex('teams')
     .then(function () {
       // Inserts seed entries
       return knex('teams').insert([
