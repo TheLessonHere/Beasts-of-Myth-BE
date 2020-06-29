be1c05d50cc3613272e2a92d7a130f2d6599c0fc
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2020-06-29 16:26:52 -0600

    Attempting to re-migrate

--- data/seeds/02-messages.js
+++ data/seeds/02-messages.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('messages')
+  return knex('messages').del()
     .then(function () {
       // Inserts seed entries
       return knex('messages').insert([
