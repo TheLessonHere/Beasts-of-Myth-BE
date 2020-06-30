85d585b7e844f2f5695cccfe048b72d72ec59315
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2020-06-29 18:10:41 -0600

    Updated Knex schema

--- data/seeds/02-messages.js
+++ data/seeds/02-messages.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('messages').truncate()
+  return knex('messages').del()
     .then(function () {
       // Inserts seed entries
       return knex('messages').insert([
