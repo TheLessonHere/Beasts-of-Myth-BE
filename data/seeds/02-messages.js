e1c57807e0bc62d1b6d786d6c07ccc3e83a9f4aa
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2020-06-29 18:03:09 -0600

    Attempting non-Postgres

--- data/seeds/02-messages.js
+++ data/seeds/02-messages.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('messages').del()
+  return knex('messages').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('messages').insert([
--- data/seeds/03-teams.js
+++ data/seeds/03-teams.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('teams').del()
+  return knex('teams').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('teams').insert([
